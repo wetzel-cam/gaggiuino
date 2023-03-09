c22f4d6d133d394361496af0779ba8c1f729aa3f
Cameron Wetzel <wetzel.cam@gmail.com>
2023-03-08 22:49:28 -0600

    ESP32 Support - Watchdog
    IWatchdog does not support ESP32 boards. Used Adafruit's SleepyDog library in place of it.
    Adafruit's SleepyDog library uses the task watchdog timer (TWDT), where IWatchdog seems to implement a interrupt watchdog timer (IWDT). May or may not be a problem...
    
    - Added conditional defines for ESP32 boards to define SleepyDog.
    - Defined similar functionality using the SleepyDog lib for ESP32 boards

--- src/peripherals/internal_watchdog.h
+++ src/peripherals/internal_watchdog.h
@@ -1,21 +1,43 @@
 #ifndef INTERNALWATCHDOG_H
 #define INTERNALWATCHDOG_H
 #include <Arduino.h>
-#include <IWatchdog.h>
+
+#ifdef ESP32 // Needed to use a different lib for the Watchdog timer
+  #define SLEEPY_DOG
+  #include <Adafruit_SleepyDog.h>
+#else
+  #define IWATCHDOG
+  #include <IWatchdog.h>
+#endif
+
 #include "../lcd/lcd.h"
 
 /*Checking whether system is booting after a hard reset initiated by the internal watchdog.*/
 static inline void iwdcInit(void) {
   // IWDC init
-  if(IWatchdog.isReset()) {
-    lcdShowPopup("WATCHDOG RESTARTED");
-    IWatchdog.clearReset();
-  }
-  IWatchdog.begin(3000000);
+  #ifdef IWATCHDOG
+    if(IWatchdog.isReset()) {
+      lcdShowPopup("WATCHDOG RESTARTED");
+      IWatchdog.clearReset();
+    }
+    IWatchdog.begin(3000000);
+  #endif
+  #ifdef SLEEPY_DOG
+    if (esp_reset_reason() == ESP_RST_TASK_WDT) {
+      lcdShowPopup("WATCHDOG RESTARTED");
+      Watchdog.disable();
+    }
+    Watchdog.enable(3000);
+  #endif
   LOG_INFO("Internal watchdog Init");
 }
 
 static inline void watchdogReload(void) {
-  IWatchdog.reload();
+  #ifdef IWATCHDOG
+    IWatchdog.reload();
+  #endif
+  #ifdef SLEEPY_DOG
+    Watchdog.reset();
+  #endif
 }
 #endif
